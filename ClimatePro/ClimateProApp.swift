b036a222edc33965c2326eeac0342c5d1dedf9df
Dmitriy Danilin <danilindn@icloud.com>
2025-03-27 13:00:02 +0300

    Initial Commit

---
+++ ClimatePro/ClimateProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClimateProApp.swift
+//  ClimatePro
+//
+//  Created by Дмитрий Данилин on 27.03.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClimateProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClimateProTests/ClimateProTests.swift
@@ -0,0 +1,16 @@
+//
+//  ClimateProTests.swift
+//  ClimateProTests
+//
+//  Created by Дмитрий Данилин on 27.03.2025.
+//
+
+import Testing
+
+struct ClimateProTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
